fbe785a88e399550854decd76b6f964f089167e1
Chimoha31 <mokochii1108@gmail.com>
2022-02-21 19:02:40 -0800

    Finally fixed bug

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 import List from "./List";
 import Form from "./Form";
+import Header from './Header';
+import {ThemeContext} from './const/contexts/ThemeContext';
 import styled from 'styled-components';
-import { Header } from "./Header";
-import { ThemeContext } from "./const/contexts/ThemeContext";
+// import {Modal} from './compoents/modal';
 
 
 function App({ data }) {
-  const [tab, setTab] = useState('list');
+  const [tab, setTab] = useState("list");
   const [langs, setLangs] = useState(data);
-  
+
   const [theme] = useContext(ThemeContext);
-  
+
   const addLang = (lang) => {
     setLangs([...langs, lang]);
     setTab('list');
-  };
-  
+  }
+
   return (
-    <Container theme={theme}>
-      <Header tab={tab} setTab={setTab}/>
-      {
-        tab === 'list' ? <List langs={langs} /> : <Form onAddLang={addLang}/>
-      }
+    <Container theme={theme}>      
+    <Header tab={tab} setTab={setTab} />
+      {tab === "list" ? <List langs={langs}/> : <Form onAddLang={addLang} />}
+      {/* modalがちゃんと効いているか見ただけ。消さないと、ずっとmodal状態 */}
+      {/* <Modal /> */}
     </Container>
   );
 }
 
 const Container = styled.div`
   height: 100%;
-  color: ${({ theme }) => theme.color};
-  background-color: ${({ theme }) => theme.backgroundColor};
+  color: ${({theme}) => theme.color};
+  background-color: ${({theme}) => theme.backgroundColor};
 `
+
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,23 @@
-import { useContext } from 'react';
-import styled from "styled-components";
-import { Button } from "./compoents/button";
-import { ThemeContext } from "./const/contexts/ThemeContext";
+import React, {useContext} from 'react';
+import styled from 'styled-components';
+import {Button}  from './compoents/button';
+import {ThemeContext} from './const/contexts/ThemeContext';
 
 
-export const Header = (props) => {
+const Header = (props) => {
   const tab = props.tab;
   const setTab = props.setTab;
 
-
   const [theme, toggleTheme] = useContext(ThemeContext);
-  return (
+
+  return(
     <Container>
-      <HeaderUl>
-        <HeaderLi focused={tab === 'list'} onClick={() => setTab('list')}>リスト</HeaderLi>
-        <HeaderLi focused={tab === 'form'} onClick={() => setTab('form')}>フォーム</HeaderLi>
-      </HeaderUl>
-      <HeaderButton onClick={toggleTheme}>テーマ変更</HeaderButton>
-    </Container>
+    <HeaderUl>
+      <HeaderLi focused={tab=== 'list'} onClick={() => setTab('list')}>List</HeaderLi>
+      <HeaderLi focused={tab === 'form'} onClick={() => setTab('form')}>Form</HeaderLi>
+    </HeaderUl>
+    <HeaderButton onClick={toggleTheme}>Theme Change</HeaderButton>
+  </Container>
   )
 }
 
@@ -27,20 +27,24 @@ const Container = styled.header`
   padding: 24px 64px 0;
   border-bottom: 1px solid #E0E0E0;
 `
-
 const HeaderUl = styled.ul`
-  display: flex;
+ display: flex;
   margin: 0;
   padding: 0;
 `
+
 const HeaderLi = styled.li`
   list-style: none;
   padding: 4px 12px;
   cursor: pointer;
-  border-bottom: ${props => props.focused ? '2px solid #F44336' : 'none' };
+  border-bottom: ${props => props.focused ? '2px solid #f44336': 'none'}
 `
 
 const HeaderButton = styled(Button)`
-  padding: 0;
-  margin-bottom: 4px;
+width: 130px;
+height: 32px;
+padding: 0;
+margin-bottom: 4px;
 `
+
+export default Header;
